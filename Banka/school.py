175b87e18fc1111f81adcb93e843b6f72f414995
Tahir Can Kozan <93403102+tahircankzn@users.noreply.github.com>
2023-04-23 11:53:45 +0300

    Update school.py

--- Banka/school.py
+++ Banka/school.py
@@ -9,14 +9,22 @@ class öğrenci:
         self.sınıf = sınıf
         self.notlar = {
             #      vize final
-            "mat" : [0 , 0],
-            "fen" : [0 , 0],
+            "matematik" : [0 , 0],
+            "Elektrik Devre Temelleri" : [0 , 0],
+            "Bilgisayar Programlama" : [0 , 0],
+            "Derin Öğrenme" : [0 , 0],
+            "Optimizasyon Yöntemleri" : [0 , 0],
+            
         }
 
     def ortalama(self):
-        mat = self.notlar["mat"]
-        fen = self.notlar["fen"]
-        return (mat[0]+mat[1])/2 , (fen[0]+fen[1])/2
+        mat = self.notlar["matematik"]
+        edt = self.notlar["Elektrik Devre Temelleri"]
+        bp = self.notlar["Bilgisayar Programlama"]
+        dö = self.notlar["Derin Öğrenme"]
+        oy = self.notlar["Optimizasyon Yöntemleri"]
+        
+        return (mat[0]+mat[1])/2 , (edt[0]+edt[1])/2 , (bp[0]+bp[1])/2, (dö[0]+dö[1])/2, (oy[0]+oy[1])/2
 
 class öğretmen:
     def __init__(self,isim,branş,şifre):
@@ -41,8 +49,8 @@ class akademikKadro:
     def __init__(self):
         
         self.liste = {
-            "123" : müdür("ali","fen","ali123"),
-            "1234" : öğretmen("ali","fen","ali123")
+            "123" : müdür("ali","Müdür","ali123"),
+            "1234" : öğretmen("ali","matematik","ali123")
 
         }
 
@@ -53,58 +61,106 @@ sınıflar = {
 }
 
 
+
+
+
+
 kadro = akademikKadro()
-öğrenciler = {"123":öğrenci("Tahir Can Kozan",56144230822,1,1234)}
+öğrenciler = {"123":öğrenci("Tahir Can Kozan",56144230822,1,56144)}
 
 sınıflar["sınıf1"].liste.append(öğrenciler)
-sınıflar["sınıf2"].liste.append({"1234":öğrenci("Ali",123456789,1,1234)})
+sınıflar["sınıf2"].liste.append({"1234":öğrenci("Ali",123456789,1,12345)})
+
+
+
+def sınıfListeleri(sınıflar):
+    for i in sınıflar:
+        print(f"{i}     Öğrenci No     İsim:\n")
+        #print("Öğrenci No     İsim\n")
+        for a in sınıflar[i].liste[0].keys():
+            print(f"              {a}         {sınıflar[i].liste[0][a].isim}")
+        print("\n")
+
+
+
 
 
 
 while True:
 
-    print("1 öğreci giriş , 2 öğretmen giriş")
+    print("1 -> Öğreci giriş\n2 -> Öğretmen giriş")
 
     a = int(input("> "))
 
     if a == 1: # 1 öğreci giriş
-        öğrenciNo = (input("> Öğrenci NO : "))
-        şifre = (input("> şifre : "))
 
-        if öğrenciler[öğrenciNo].şifre == şifre:
-            print("giriş oldu")
-        öğrenci1 = öğrenciler[öğrenciNo]
-        while True:
-            print("1 not bak ,2 not hesapla")
+        
+        doğrulama = 0
+        for i in range(3):
+
+            öğrenciNo = (input("> Öğrenci NO : "))
+            şifre = int(input("> Şifre : "))
+            
+            
+            try:
+                if öğrenciler[öğrenciNo].şifre == şifre:
+                    
+                    doğrulama = 1
+                    öğrenci1 = öğrenciler[öğrenciNo]
+                    
+                    break
+                
+                    
+            except KeyError:
+                print("Öğrenci No hatalı !!")
+
+            if i==2:
+                    print("giriş sayısı aşıldı")
+
+
+
+        while doğrulama:
+            print("1 -> Notlarım\n2 -> Not hesaplama")
             k = input(">")
 
             if k == "1":
                 print(öğrenci1.notlar)
 
             elif k == "2":
-                print("1 ortalamarı hesapla ,2 elle hesapla")
+                print("1 -> Ortalamarı hesapla\n2 -> Elle hesapla")
                 l = input(">")
 
                 if l ==  "1":
-                    mat , fen = öğrenci1.ortalama()
-                    print(mat,fen)
+                    mat , edt , bp , dö , oy = öğrenci1.ortalama()
+                    print(f"Matematik -> {mat}\nElektrik Devre Temelleri -> {edt}")
+                    print(f"Bilgisayar Programlama -> {bp}\nDerin Öğrenme -> {dö}")
+                    print(f"Optimizasyon Yöntemleri -> {oy}")
+
+
 
                 elif l ==  "2":
                     not1 = float(input(">1. not : "))
                     not2 = float(input(">2. not : "))
                     print(f"ortalama { (not1+not2)/2}")
 
+
+###########################################################################################
     elif a ==2: # 2 öğretmen giriş
         doğrulama = 0
+        Müdür = 0
+        Öğretmen_branşı = None
         for i in range(3):
 
             giriş = input("Öğretmen No : ")
-            şifre = input("şifre : ")
-            print(i)
+            şifre = input("Şifre : ")
+            
             
             try:
                 if kadro.liste[giriş].şifre == şifre:
+                    if kadro.liste[giriş].branş == "Müdür":
+                        Müdür = 1
                     doğrulama = 1
+                    Öğretmen = kadro.liste[giriş].branş
                     break
                 
                     
@@ -116,70 +172,110 @@ while True:
                     
         
         if doğrulama == 1:
-            
 
-            print("1 öğrenci işlemleri,2 sınav işlemleri")
 
-            s = input("> ")
 
-            if s =="1":
 
-                print("1 öğrenci ekle , 2 öğrenci sil, 3 sınav notu gir, 4 sınıf listesi düzenle")
-
-                r = input("> ")
-
-                if r == "1":
-                    y = input("> isim - tc - sınıf")
-                    liste1 = list(y.split(" "))
-                    öğrenciNo = None
+            if Müdür == 0: # öğretmen
+                while True:
+                    print("1 -> Sınav notu gir\n2 -> Sınıf listesi düzenle\n3 -> Sınıf Listeleri\n4 -> Çıkış")
                     while True:
-                        öğrenciNo = random.randint(1,100)
-                        if öğrenciNo not in öğrenciler.keys():
+                        r = input("> ")
+                        if r in ["1","2","3"]:
                             break
-                    öğrenciler[str(öğrenciNo)] = öğrenci(liste1[0],liste1[1],liste1[2],liste1[1][0:5])
-                    print(öğrenciNo,liste1[1][0:5])
+                        else:
+                            print("Geçersiz Seçim")
+
+                    if r == "1":
+                        for i in öğrenciler:
+                            print(öğrenciler[i].isim,i)
+
+                            
+
+                            Not1 = input(f"> {Öğretmen_branşı} vize -> ")
+                            Not2 = input(f"> {Öğretmen_branşı} final -> ")
+                            
+                            öğrenciler[i].notlar[Öğretmen_branşı] = [Not1,Not2]
+                        
+
+
+
+
+                    elif r == "2":
+                        for i in sınıflar:
+                            print(f"{i} :")
+                            for a in sınıflar[i].liste:
+                                [print(x) for x in a.keys()]
+                        print("Sınıf değişikliği için öğrenci no ve sınıf seçiniz.")
+                        öğrenciNo = input("> Öğrenci No : ")
+                        ilkSınıf = input("> nerden : ")
+                        ikinciSınıf = input("> nereye : ")
+                        öğrenci_değişim = sınıflar[ilkSınıf]
+                        del sınıflar[ilkSınıf].liste[0][öğrenciNo]
+                        sınıflar[ikinciSınıf].liste[0][öğrenciNo] = öğrenci_değişim
+                        print(sınıflar["sınıf1"].liste[0])
+                        print(sınıflar["sınıf2"].liste[0])
+                        pass
+
+                    elif r == "3":
+                        sınıfListeleri(sınıflar)
+
+
+                    elif r == "4":
+                        break
+
+
+
+
+
+            else: # müdür
+                # öğretmen ekle sil , sınıf aç sil
+                while True:
+                    print("1 -> Öğrenci ekle\n2 -> Öğrenci sil\n3 -> Sınıf Listeleri\n4 -> Çıkış")
+
+
+                    while True:
+                        r = input("> ")
+                        if r in ["1","2","3"]:
+                            break
+                        else:
+                            print("Geçersiz Seçim")
+
+
+
+                    if r == "1":
+                        y = input("> isim - tc - sınıf\n")
+                        liste1 = list(y.split(" "))
+                        öğrenciNo = None
+                        while True:
+                            öğrenciNo = random.randint(1,100)
+                            if öğrenciNo not in öğrenciler.keys():
+                                break
+                        öğrenciler[str(öğrenciNo)] = öğrenci(liste1[0],liste1[1],liste1[2],liste1[1][0:5])
+
+                        # liste1[2] bu sınıfa eklenmeli
+
+                        sınıflar[liste1[2]].liste[0][öğrenciNo] = öğrenciler[str(öğrenciNo)]
+
+
+
+                        print(f"Öğrenci No : {öğrenciNo}\nŞifre : {liste1[1][0:5]}")
                     
                     
                     
-                elif r == "2":
-                    s = input("silmek istediğniz öğrenci no :")
-                    del öğrenciler[s]
+                    elif r == "2":
+                        s = input("Silmek istediğniz öğrenci no :")
+                        del öğrenciler[s]
+                        for i in sınıflar:
+                            if s in sınıflar[i].liste:
+                                del sınıflar[i].liste[s]    
+                        pass
+                    
+                    elif r == "3":
+                        sınıfListeleri(sınıflar)
 
-                    for i in sınıflar:
-                        if s in sınıflar[i].liste:
-                            del sınıflar[i].liste[s]    ############
+                    elif r == "4":
+                        break
 
-
-                elif r == "3":
-                    for i in öğrenciler:
-                        print(öğrenciler[i].isim,i)
-                        mat1 = input("> mat vize")
-                        mat2 = input("> mat final")
-
-                        fen1 = input("> fen vize")
-                        fen2 = input("> fen final")
-
-                        öğrenciler[i].notlar["mat"] = [mat1,mat2]
-                        öğrenciler[i].notlar["fen"] = [fen1,fen2]
-
-                elif r == "4":
-                    for i in sınıflar:
-                        print(f"{i} :")
-                        for a in sınıflar[i].liste:
-                            [print(x) for x in a.keys()]
-
-                    print("Sınıf değişikliği için öğrenci no ve sınıf seçiniz.")
-
-                    öğrenciNo = input("> Öğrenci No : ")
-                    ilkSınıf = input("> nerden : ")
-                    ikinciSınıf = input("> nereye : ")
-
-                    öğrenci_değişim = sınıflar[ilkSınıf]
-
-                    del sınıflar[ilkSınıf].liste[0][öğrenciNo]
-
-                    sınıflar[ikinciSınıf].liste[0][öğrenciNo] = öğrenci_değişim
-                    print(sınıflar["sınıf1"].liste[0])
-                    print(sınıflar["sınıf2"].liste[0])
 
 
